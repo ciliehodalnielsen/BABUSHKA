0ce99b97280dd928d5d97fa2bb17b471f98399c4
Cilie Hodal Nielsen <ciliehodalnielsen@meyer.kea.local>
2018-09-03 11:25:09 +0200

    menuen virker nu

---
+++ javascript.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", hentJson);
+
+
+let dest = document.querySelector(".data-container"),
+    retter,
+    kategoriFilter = "alle";
+
+
+async function hentJson() {
+    let jsonData = await fetch("json/menu.json");
+    retter = await jsonData.json();
+
+    //console.log(retter);
+
+    visRetter();
+}
+
+document.querySelectorAll(".menu-item").forEach(knap => {
+
+
+    //console.log(knap);
+    knap.addEventListener("click", filtering)
+
+});
+
+function filtering() {
+
+
+
+    kategoriFilter = this.getAttribute("data-kategori");
+    //console.log(kategoriFilter);
+    visRetter();
+
+
+
+}
+
+function visRetter() {
+    let temp = document.querySelector(".retter-template");
+    let modtager = document.querySelector(".main-container");
+    modtager.textContent = "";
+    document.querySelector("header h3").textContent = kategoriFilter;
+    retter.forEach(ret => {
+        console.log(ret.kategori, kategoriFilter);
+        if (ret.kategori == kategoriFilter || kategoriFilter == "alle") {
+
+            console.log(ret.kategori, kategoriFilter);
+            let klon = temp.cloneNode(true).content;
+            klon.querySelector(".data-navn").textContent = ret.navn;
+            klon.querySelector(".data-billede").src = "imgs/small/" + ret.billede + "-sm.jpg";
+            klon.querySelector(".data-billede").alt = "billede" + ret.navn;
+            klon.querySelector(".data-kortbeskrivelse").textContent = ret.kortbeskrivelse;
+            klon.querySelector(".data-pris").textContent = ret.pris;
+            modtager.appendChild(klon);
+        }
+    });
+}
